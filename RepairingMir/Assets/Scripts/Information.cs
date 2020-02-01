d7f455ca0dea716883c7a32514690c9916a7cb5e
Luc <lucholland74@gmail.com>
2020-02-01 20:25:15 +1100

    working on menu/ cutscene

---
+++ RepairingMir/Assets/Scripts/Information.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Information : MonoBehaviour{
+
+
+    public GameObject Info;
+
+    public void OpenPanel()
+    {
+        if(Info != null)
+        {
+            bool isActive = Info.activeSelf;
+
+            Info.SetActive(!isActive);
+        }
+    }
+
+
+
+}
---
+++ RepairingMir/Assets/Scripts/Pause.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour{
+
+
+    public GameObject CanvasPaused;
+
+    public void OpenPanel()
+    {
+        if (CanvasPaused != null)
+
+        {
+            bool isActive = CanvasPaused.activeSelf;
+
+            CanvasPaused.SetActive(!isActive);
+        }
+    
+    }
+
+
+
+}
