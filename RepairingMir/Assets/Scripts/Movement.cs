d7060830aad053ad631580d38fa78ecf32d423f9
Holiday Course <holiday@ad.aie.edu>
2020-02-01 11:15:28 +1100

    Adding the movement scene/script

---
+++ RepairingMir/Assets/Scripts/Movement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float MaxSpeed = 1;
+    public float RollAccel = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody ourBody = GetComponent<Rigidbody>();
+        if (Input.GetAxis("Roll") != 0.0f)
+        {
+            ourBody.angularVelocity += ourBody.transform.forward * (-Input.GetAxis("Roll") * Time.fixedDeltaTime) * RollAccel;
+        }
+        if (Input.GetAxis("Pitch") != 0.0f)
+        {
+            ourBody.angularVelocity += ourBody.transform.right * (Input.GetAxis("Pitch") * Time.fixedDeltaTime) * RollAccel;
+        }
+        if (Input.GetAxis("Yaw") != 0.0f)
+        {
+            ourBody.angularVelocity += ourBody.transform.up * (Input.GetAxis("Yaw") * Time.fixedDeltaTime) * RollAccel;
+        }
+        Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+        bool vert = Input.GetAxis("Vertical") != 0.0f;
+        bool hori = Input.GetAxis("Horizontal") != 0.0f;
+        bool up = Input.GetAxis("Boost") != 0.0f;
+
+        if (vert)
+        {
+            //Adding velocity
+            movement += ourBody.transform.forward * (Input.GetAxis("Vertical"));
+        }
+        if (hori)
+        {
+            //Adding velocity
+            movement += ourBody.transform.right * Input.GetAxis("Horizontal");
+        }
+        if (up)
+        {
+            movement += ourBody.transform.up * Input.GetAxis("Boost");
+        }
+        if (movement.sqrMagnitude != 0.0f)
+        {
+            movement = movement * Time.fixedDeltaTime * MaxSpeed;
+            if (movement.magnitude > 1)
+            {
+                movement.Normalize();
+            }
+            if (ourBody.velocity.magnitude > movement.magnitude)
+            {
+                movement -= (ourBody.velocity.normalized * (movement.magnitude / 4));
+            }
+            ourBody.velocity += movement;
+        }
+    }
+
+
+}
