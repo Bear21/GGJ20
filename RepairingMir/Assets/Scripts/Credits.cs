a0294a9377055b2726cbce2ae0532c33ece12290
Luc <lucholland74@gmail.com>
2020-02-01 22:02:43 +1100

    Added End cut scene flame

---
+++ RepairingMir/Assets/Scripts/Credits.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour{
+
+
+    public GameObject CanvasCredits;
+
+    public void CreditsCanvas()
+    {
+        if (CanvasCredits != null)
+
+        {
+            bool isActive = CanvasCredits.activeSelf;
+
+            CanvasCredits.SetActive(!isActive);
+        }
+    
+    }
+
+
+
+}
