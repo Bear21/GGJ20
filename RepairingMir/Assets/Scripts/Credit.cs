94f6626ed97c8006b2ceb743ad77078df63925ed
luc <lucholland74@gmail.com>
2020-02-03 16:28:47 +1100

    ui changes/ fixes. additional 02 bar that goes down with timer

---
+++ RepairingMir/Assets/Scripts/Credit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour{
+
+
+    public GameObject People;
+
+    public void OpenPanel()
+    {
+        if(People != null)
+        {
+            bool isActive = People.activeSelf;
+
+            People.SetActive(!isActive);
+        }
+    }
+
+
+
+}
---
+++ RepairingMir/Assets/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Image o2forground;
+    public float maxTime = 5f;
+    float timeLeft;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        o2forground = GetComponent<Image>();
+        timeLeft = maxTime; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            o2forground.fillAmount = timeLeft / maxTime;
+
+        }
+    }
+}
